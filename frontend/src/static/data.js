9b3e89d7f65aef145fec4c8c35f6d8b36db7a113
fahad <mfahad1667@gmail.com>
2024-09-07 15:23:10 +0500

    store static images in s3 bucket

--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -194,69 +194,69 @@ export const navItems = [
       title: "Computers and Laptops",
       subTitle: "",
       image_Url:
-        "https://cdn.shopify.com/s/files/1/1706/9177/products/NEWAppleMacbookProwithM1ProChip14InchLaptop2021ModelMKGQ3LL_A_16GB_1TBSSD_custommacbd.jpg?v=1659592838",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/computers-laptop.png",
     },
     {
       id: 2,
       title: "cosmetics and body care",
       subTitle: "",
       image_Url:
-        "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-07/kosme1.png",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/cosmetics.png",
     },
     {
       id: 3,
       title: "Accesories",
       subTitle: "",
       image_Url:
-        "https://img.freepik.com/free-vector/ordering-goods-online-internet-store-online-shopping-niche-e-commerce-website-mother-buying-babies-clothes-footwear-toys-infant-accessories_335657-2345.jpg?w=2000",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/Accesories.png",
     },
     {
       id: 4,
       title: "Cloths",
       subTitle: "",
       image_Url:
-        "https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/clothes.png",
     },
     {
       id: 5,
       title: "Shoes",
       subTitle: "",
       image_Url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/shoes.png",
     },
     {
       id: 6,
       title: "Gifts",
       subTitle: "",
       image_Url:
-        "https://securecdn.pymnts.com/wp-content/uploads/2014/11/Gifts-Photo-700x489.jpg",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/gifts.png",
     },
     {
       id: 7,
       title: "Pet Care",
       subTitle: "",
-      image_Url: "https://cdn.openpr.com/T/c/Tc15444071_g.jpg",
+      image_Url: "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/pets-care.png",
     },
     {
       id: 8,
       title: "Mobile and Tablets",
       subTitle: "",
       image_Url:
-        "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/mobile-tablet.png",
     },
     {
       id: 9,
       title: "Music and Gaming",
       subTitle: "",
       image_Url:
-        "https://static.vecteezy.com/system/resources/previews/011/996/555/original/3d-black-headphone-illustration-ecommerce-icon-png.png",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/music-gaming.png",
     },
     {
       id: 10,
       title: "Others",
       subTitle: "",
       image_Url:
-        "https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png",
+        "https://b2bazar-bucket.s3.us-west-2.amazonaws.com/others.png",
     },
   ];
   
