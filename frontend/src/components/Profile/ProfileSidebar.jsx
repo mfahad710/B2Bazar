00d4fb33f5e5c626cd59fc48545b5c06c0e38b84
fahad <mfahad1667@gmail.com>
2024-09-08 11:11:02 +0500

    modify profile sidebar - 08/09/2024

--- frontend/src/components/Profile/ProfileSidebar.jsx
+++ frontend/src/components/Profile/ProfileSidebar.jsx
@@ -31,7 +31,7 @@ const ProfileSidebar = ({ setActive, active }) => {
       });
   };
   return (
-    <div className="w-full bg-white shadow-sm rounded-[10px] p-4 pt-8">
+    <div className="w-full bg-[#FEFFFF] shadow-sm rounded-[10px] p-4 pt-8">
       <div
         className="flex items-center cursor-pointer w-full mb-8"
         onClick={() => setActive(1)}
